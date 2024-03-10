4066be693e734eec99a8319656c37c81b79a5fe6
Daniel Martins Antunes <danoan2008@gmail.com>
2024-03-10 14:47:55 +0100

    Improve github workflows

--- docs/conf.py
+++ docs/conf.py
@@ -7,20 +7,21 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 # Add the package source to the sys.path
+import sphinx_rtd_theme
+from importlib import metadata
 import pathlib
 import sys
 
-source_folder = pathlib.Path(__file__).parents[1].joinpath("src/danoan").resolve().as_posix()
+source_folder = pathlib.Path(__file__).parents[1].joinpath(
+    "src/danoan").resolve().as_posix()
 sys.path.insert(0, source_folder)
 
 # Tell sphinx the package version
-from importlib import metadata
 
 PACKAGE_VERSION = metadata.version("toml-dataclass")
 version = release = PACKAGE_VERSION
 
 # Import read the docs theme
-import sphinx_rtd_theme
 
 project = "toml-dataclass"
 copyright = "2023, Daniel Martins Antunes"
@@ -47,7 +48,6 @@ autodoc_typehints = "both"
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 
 
 # -- Options for HTML output -------------------------------------------------
